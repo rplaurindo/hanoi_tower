d0848ee7c8e5ff266ebd21e3b3ce64ef86cf132a
Rafael Laurindo <rplaurindo>
2016-11-28 18:34:20 -0200

    add comment

--- Input.java
+++ Input.java
@@ -1,9 +1,11 @@
+// for don’t need call the complete naming of the class "Scanner"
 import java.util.Scanner;
 
 public class Input {
     public String keyboard(String label) {
         System.out.print(label);
         Scanner input = new Scanner(System.in);
+        // java.util.Scanner input = new java.util.Scanner(System.in);
 
         return input.next();
     }
