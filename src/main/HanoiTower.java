c848f259930de996e3ec48343c9499d159c5136b
Rafael Laurindo <rplaurindo>
2016-12-27 11:48:37 -0200

    refactor

--- src/main/HanoiTower.java
+++ src/main/HanoiTower.java
@@ -147,3 +147,23 @@ public class HanoiTower {
     }
 
 }
+
+class Main {
+    
+    public static void main(String... args) throws Exception {
+        Input input = new Input();
+        int disksCount = input.catchInteger();
+        HanoiTower h = new HanoiTower(disksCount);
+        h.run();
+        
+        System.out.print("\n");
+        for (Movement movement : h.movements()) {
+            System.out.println("Movimento " + movement.step() + ": " +
+                               "mova o disco " + movement.disk().index() +
+                               " da haste " + movement.origin() +
+                               " para a haste " + movement.destination()
+                              );
+        }
+    }
+    
+}
--- src/main/Rod.java
+++ src/main/Rod.java
@@ -6,11 +6,22 @@ public class Rod {
     private char label;
     private List<Disk> disks;
 
-    Rod(char label) {
+    Rod(char label, Disk... disks) {
         this.label  = label;
+//        List<Disk> initDiskList = new ArrayList<Disk>(disks);
+//        this.disks = (ArrayList<Disk>) disks;
     }
     
-    public char label () {
+    public char label() {
         return label;
+    }
+    
+    public void addDisk(Disk disk) {
+        disks.add(disk);
+    }
+    
+    public void moveDisk(Disk disk, Rod rod) {
+        disks.remove(disk);
+        rod.addDisk(disk);
     }
 }
